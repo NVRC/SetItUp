89d692b163e33af8d0581a28e17dcee6ab8dca07
NVRC <nathaniel.charlebois@gmail.com>
2018-05-04 15:07:23 -0400

    Contacts parsing with ListView, awkward permissions

---
+++ app/src/main/java/threeblindmice/setitup/Contacts.java
@@ -0,0 +1,155 @@
+package threeblindmice.setitup;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.widget.Toast;
+
+
+public class Contacts extends AppCompatActivity {
+
+    private SimpleCursorAdapter adapter;
+    // Defines the id of the loader for later reference
+    public static final int CONTACT_LOADER_ID = 78;
+    // From docs: A unique identifier for this loader. Can be whatever you want.
+
+    // Identifier for the permission request
+    private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        getPermissionToReadUserContacts();
+
+        setupCursorAdapter();
+        // Find list and bind to adapter
+        ListView lvContacts = (ListView) findViewById(R.id.lvContacts);
+        lvContacts.setAdapter(adapter);
+
+        // Initialize the loader with a special ID and the defined callbacks from above
+        getSupportLoaderManager().initLoader(CONTACT_LOADER_ID,
+                new Bundle(), contactsLoader);
+
+
+
+    }
+
+    private void setupCursorAdapter(){
+        // Column data from cursor to bind views from
+        String[] uiBindFrom = { ContactsContract.Contacts.DISPLAY_NAME };
+        // View IDs which will have the respective column data inserted
+        int[] uiBindTo = { R.id.tvName };
+        // Create the simple cursor adapter to use for our list
+        // specifying the template to inflate (item_contact),
+        adapter = new SimpleCursorAdapter(
+                this, R.layout.item_contact,
+                null, uiBindFrom, uiBindTo,
+                0);
+    }
+
+    // Defines the asynchronous callback for the contacts data loader
+    private LoaderManager.LoaderCallbacks<Cursor> contactsLoader =
+            new LoaderManager.LoaderCallbacks<Cursor>() {
+                // Create and return the actual cursor loader for the contacts data
+                @Override
+                public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                    // Define the columns to retrieve
+                    String[] projectionFields = new String[] { ContactsContract.Contacts._ID,
+                            ContactsContract.Contacts.DISPLAY_NAME};
+                    // Construct the loader
+                    CursorLoader cursorLoader = new CursorLoader(Contacts.this,
+                            ContactsContract.Contacts.CONTENT_URI, // URI
+                            projectionFields, // projection fields
+                            null, // the selection criteria
+                            null, // the selection args
+                            null // the sort order
+                    );
+                    // Return the loader for use
+                    return cursorLoader;
+                }
+
+                // When the system finishes retrieving the Cursor through the CursorLoader,
+                // a call to the onLoadFinished() method takes place.
+                @Override
+                public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+                    // The swapCursor() method assigns the new Cursor to the adapter
+                    adapter.swapCursor(cursor);
+                }
+
+                // This method is triggered when the loader is being reset
+                // and the loader data is no longer available. Called if the data
+                // in the provider changes and the Cursor becomes stale.
+                @Override
+                public void onLoaderReset(Loader<Cursor> loader) {
+                    // Clear the Cursor we were using with another call to the swapCursor()
+                    adapter.swapCursor(null);
+                }
+            };
+
+    // Called when the user is performing an action which requires the app to read the
+    // user's contacts
+    public void getPermissionToReadUserContacts() {
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
+        // checking the build version since Context.checkSelfPermission(...) is only available
+        // in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted)
+        // since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // The permission is NOT already granted.
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show our own UI to explain to the user why we need to read the contacts
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_CONTACTS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    // Callback with the request from calling requestPermissions(...)
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read Contacts permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                // showRationale = false if user clicks Never Ask Again, otherwise true
+                boolean showRationale = shouldShowRequestPermissionRationale( Manifest.permission.READ_CONTACTS);
+
+                if (showRationale) {
+                    // do something here to handle degraded mode
+                } else {
+                    Toast.makeText(this, "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/threeblindmice/setitup/ContactsLoader.java
@@ -0,0 +1,58 @@
+package threeblindmice.setitup;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.Adapter;
+
+/**
+ * Created by Slate on 2018-05-04.
+ */
+
+public class ContactsLoader implements LoaderManager.LoaderCallbacks<Cursor> {
+    private Contacts contacts;
+    private SimpleCursorAdapter sAdapter;
+    public ContactsLoader(Contacts source, SimpleCursorAdapter adapter){
+        contacts = source;
+        sAdapter = adapter;
+    }
+
+    // Create and return the actual cursor loader for the contacts data
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Define the columns to retrieve
+        String[] projectionFields = new String[] { ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME};
+        // Construct the loader
+        CursorLoader cursorLoader = new CursorLoader(contacts,
+                ContactsContract.Contacts.CONTENT_URI, // URI
+                projectionFields, // projection fields
+                null, // the selection criteria
+                null, // the selection args
+                null // the sort order
+        );
+        // Return the loader for use
+        return cursorLoader;
+    }
+
+    // When the system finishes retrieving the Cursor through the CursorLoader,
+    // a call to the onLoadFinished() method takes place.
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // The swapCursor() method assigns the new Cursor to the adapter
+        sAdapter.swapCursor(cursor);
+    }
+
+    // This method is triggered when the loader is being reset
+    // and the loader data is no longer available. Called if the data
+    // in the provider changes and the Cursor becomes stale.
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Clear the Cursor we were using with another call to the swapCursor()
+        sAdapter.swapCursor(null);
+    }
+}
